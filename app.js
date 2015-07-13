56592c959def64e3cc9c5db6dbd5d3cb18641c60
unknown <selva.kumar@INCINDW003.in.increvcorp.com>
2015-07-13 16:44:50 +0530

    Floating label for Angular-Material Chips control with Autocomplete
    Implementation of Floating label for Angular-Material Chips control with
    Autocomplete

---
+++ app.js
@@ -0,0 +1,72 @@
+﻿(function () {
+    'use strict';
+    angular
+        .module('MyApp', ['ngMaterial'])
+        .controller('CustomInputDemoCtrl', DemoCtrl);
+
+    function DemoCtrl($timeout, $q) {
+        var self = this;
+
+        self.readonly = false;
+        self.selectedItem = null;
+        self.searchText = null;
+        self.querySearch = querySearch;
+        self.cities = loadCities();
+        self.selectedCities = [];
+        self.numberChips = [];
+        self.numberChips2 = [];
+        self.numberBuffer = '';
+
+        /**
+         * Search for vegetables.
+         */
+        function querySearch(query) {
+            var results = query ? self.cities.filter(createFilterFor(query)) : [];
+            return results;
+        }
+
+        /**
+         * Create filter function for a query string
+         */
+        function createFilterFor(query) {
+            var lowercaseQuery = angular.lowercase(query);
+
+            return function filterFn(city) {
+                return (city._lowername.indexOf(lowercaseQuery) === 0) ||
+                    (city._lowertype.indexOf(lowercaseQuery) === 0);
+            };
+
+        }
+
+        function loadCities() {
+            var cities = [
+              {
+                  'name': 'London',
+                  'country': 'United Kingdom'
+              },
+              {
+                  'name': 'California',
+                  'country': 'United States of America'
+              },
+              {
+                  'name': 'New Delhi',
+                  'country': 'India'
+              },
+              {
+                  'name': 'Mumbai',
+                  'country': 'India'
+              },
+              {
+                  'name': 'Chennai',
+                  'country': 'India'
+              }
+            ];
+
+            return cities.map(function (city) {
+                city._lowername = city.name.toLowerCase();
+                city._lowertype = city.country.toLowerCase();
+                return city;
+            });
+        }
+    }
+})();
