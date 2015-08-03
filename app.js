fc8b0b750b5738ee9b54171be2082367f8041326
Selva Kumar M <mightyselva@gmail.com>
2015-08-03 13:23:03 +0530

    Page added to display Issue

--- app.js
+++ app.js
@@ -1,6 +1,98 @@
 ﻿var MyApp = angular.module('MyApp', ['ngMaterial'])
+.config(function ($mdThemingProvider) {
+    $mdThemingProvider.theme('default')
+      .primaryPalette('amber')
+      .accentPalette('orange');
+});
+MyApp.controller('InputDemoCtrl', function CtrlDemo($timeout, $q) {
+    var self = this;
+    self.readonly = false;
+    self.selectedItem = null;
+    self.searchText = null;
+    self.querySearch = querySearch;
+    self.cities = loadCities();
+    self.selectedCities = [];
+    self.numberChips = [];
+    self.numberChips2 = [];
+    self.numberBuffer = '';
 
-MyApp.controller('CustomInputDemoCtrl', function DemoCtrl($timeout, $q) {
+    /**
+     * Search for Cities.
+     */
+    function querySearch(query) {
+        var results = query ? self.cities.filter(createFilterFor(query)) : [];
+        return results;
+    }
+
+    /**
+     * Create filter function for a query string
+     */
+    function createFilterFor(query) {
+        var lowercaseQuery = angular.lowercase(query);
+
+        return function filterFn(city) {
+            return (city._lowername.indexOf(lowercaseQuery) === 0) ||
+                (city._lowertype.indexOf(lowercaseQuery) === 0);
+        };
+
+    }
+
+    function loadCities() {
+        var cities = [
+          {
+              'name': 'London',
+              'country': 'United Kingdom'
+          },
+          {
+              'name': 'California',
+              'country': 'United States of America'
+          },
+          {
+              'name': 'New Delhi',
+              'country': 'India'
+          },
+          {
+              'name': 'Mumbai',
+              'country': 'India'
+          },
+          {
+              'name': 'Chennai',
+              'country': 'India'
+          },
+          {
+              'name': 'New york',
+              'country': 'United States of America'
+          },
+          {
+              'name': 'Melbourne',
+              'country': 'Australia'
+          },
+          {
+              'name': 'Bangalore',
+              'country': 'India'
+          },
+          {
+              'name': 'Bay Area',
+              'country': 'United States of America'
+          },
+          {
+              'name': 'Lucknow',
+              'country': 'India'
+          }
+        ];
+
+        return cities.map(function (city) {
+            city._lowername = city.name.toLowerCase();
+            city._lowertype = city.country.toLowerCase();
+            return city;
+        });
+    }
+});
+
+
+var MyCustomApp = angular.module('MyCustomApp', ['ngMaterial'])
+
+MyCustomApp.controller('CustomInputDemoCtrl', function CustomCtrlDemo($timeout, $q) {
     var self = this;
     self.readonly = false;
     self.selectedItem = null;
